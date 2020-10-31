e48e3af14cbbacec29c52178dce6968bb622de20
Danny White <hi@dannywhite.site>
2020-10-31 20:06:57 +1100

    basic text-based airtable tweets

--- server.js
+++ server.js
@@ -1,26 +1,83 @@
 // Get env variables up and running
 require("dotenv").config();
 
+// Allow for scheduling
+import schedule from 'node-schedule'
+
 // Initiate Twit
 const Twit = require('twit');
 const T = new Twit({
-    consumer_key: process.env.API_KEY,
-    consumer_secret: process.env.API_SECRET_KEY,
-    access_token: process.env.ACCESS_TOKEN,
-    access_token_secret: process.env.ACCESS_SECRET
+    consumer_key: process.env.TWITTER_API_KEY,
+    consumer_secret: process.env.TWITTER_API_SECRET_KEY,
+    access_token: process.env.TWITTER_ACCESS_TOKEN,
+    access_token_secret: process.env.TWITTER_ACCESS_SECRET
 });
 
-// Run instantly
-// tweetIt();
-// Then run again every hour
-setInterval(tweetIt, 1000 * 60 * 60);
 
-// Define tweet
-function tweetIt() {
-    const r = Math.floor(Math.random() * 100);
+// Get Airtable going
+const base = require("airtable").base(process.env.AIRTABLE_BASE_ID);
+const table = "Main";
+
+const allRecords = []
+
+base(table).select({
+    // Selecting the first 3 records in Grid:
+    // maxRecords: 3,
+    view: "Grid",
+    filterByFormula: "({hidden}= '')", // Only show items that are not hidden
+    sort: [{ field: "date", direction: "desc" }], // Overrides what's set in the above view, just in case I forget
+}).eachPage(function page(records, fetchNextPage) {
+    // This function (`page`) will get called for each page of records.
+
+    records.forEach(function (record) {
+        // console.log('Retrieved', record.get('name'));
+        allRecords.push(record);
+    });
+
+
+    // To fetch the next page of records, call `fetchNextPage`.
+    // If there are more records, `page` will get called again.
+    // If there are no more records, `done` will get called.
+    fetchNextPage();
+
+}, function done(err) {
+    if (err) { console.error(err); return; }
+    // If successful...
+    // Get a random record for later tweeting
+    const randomRecord = allRecords[Math.floor(Math.random() * allRecords.length)];
+
+    const randomRecordName = randomRecord.get("name");
+    const randomRecordDate = randomRecord.get("date");
+    const randomRecordImage = randomRecord.get("images")[0].url;
+    // TODO: convert randomRecordImage to base64 for tweeting
+
+    const randomRecordString = `${randomRecordName} from ${randomRecordDate}`
+
+
+
+    // Run instantly
+    // tweetIt(randomRecordString);
+    // Then run again every hour
+    // setInterval(tweetIt(randomRecordString), 1000 * 60 * 60);
+    // Run every day at 8am
+    schedule.scheduleJob("0 0 8 1/1 * ? *", function () {
+        tweetIt(randomRecordString);
+    })
+
+});
+
+// TODO: random ephemera of the week
+
+
+function tweetIt(tweetText, tweetImage) {
+    // const r = Math.floor(Math.random() * 100);
     // Insert tweet
     const tweet = {
-        status: `Random number of the hour is ${r}`
+        // status: `Random number of the hour is ${r}`
+        status: tweetText,
+        // media_ids: [tweetImage]
+        // lat: 37.7821120598956,
+        // long: -122.400612831116
     }
 
     console.log(tweet);
@@ -51,6 +108,3 @@ function tweetIt() {
         }
     }
 }
-
-
-tweetIt();
