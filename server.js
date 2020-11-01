a26636c05ee774ffa0bf04a181d115536a7493b9
Danny White <hi@dannywhite.site>
2020-11-01 17:53:58 +1100

    working text on image

--- server.js
+++ server.js
@@ -4,12 +4,8 @@ require("dotenv").config();
 // Allow for scheduling tweets
 const schedule = require('node-schedule');
 
-// Allow for editing images
+// Allow for image editing
 const Jimp = require('jimp');
-
-// Allow for image conversion
-// TODO; do this straight from Jimp
-const imageToBase64 = require('image-to-base64');
 
 // Initiate Twit
 const Twit = require('twit');
@@ -38,7 +34,6 @@ function tweetRandomAirtableRecord() {
             allRecords.push(record);
         });
 
-
         // To fetch the next page of records, call `fetchNextPage`.
         // If there are more records, `page` will get called again.
         // If there are no more records, `done` will get called.
@@ -50,9 +45,17 @@ function tweetRandomAirtableRecord() {
         // Select a random record for tweeting
         const record = allRecords[Math.floor(Math.random() * allRecords.length)];
         const recordName = record.get("name");
+
         const recordDate = record.get("date");
+        // Format date so it can be later programatically changed
+        // Pass through the year, month, and day
+        const recordDateStructured = new Date(recordDate.substring(0, 4), (recordDate.substring(5, 7) - 1), recordDate.substring(8, 10));
+        // Use this to create a readable date format
+        const recordDateHuman = recordDateStructured.toLocaleString("default", { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' })
+
         const recordLocation = record.get("location");
         const recordCountry = record.get("country");
+        const recordLocationAndCountry = `${recordLocation}, ${recordCountry}`
         const recordTags = record.get("tags");
         // Remove dashes/hyphens from each tag array item
         const recordTagsDashed = recordTags.map(i => i.replace(/-/g, ""));
@@ -61,32 +64,22 @@ function tweetRandomAirtableRecord() {
         // Format this array into one comma-separated string
         const recordTagsFormatted = recordTagsHashed.join(", ");
 
-        const recordString = `${recordName}. ${recordLocation}, ${recordCountry}. ${recordDate}. Tagged with ${recordTagsFormatted}.`
+        const recordString =
+            `${recordName}. ${recordLocationAndCountry}. ${recordDateHuman}. Tagged with ${recordTagsFormatted}.`
 
         const recordImageUrl = record.get("images")[0].url;
 
 
-        // Compose image
-        imageToBase64(recordImageUrl).then((response) => {
-            console.log(response.substring(0, 50))
-        })
-        prepareImage(recordImageUrl)
+        prepareImage(recordImageUrl, recordDate, recordLocationAndCountry, recordName, recordTagsHashed)
             .then((value) => {
-                // Convert recordImageUrl to base64 for tweeting
-
-                // imageToBase64(value)
-                console.log(value.substring(0, 50));
-                // tweetIt(recordString, value);
+                // Trim off extraenous bits that Jimp adds to base64
+                const recordImage = value.substring(23, value.length);
+                // Prepare for tweeting
+                tweetIt(recordString, recordImage);
             })
-        // .then((response) => {
-        //     // Image is now converted, prepare tweet
-        //     // Get this to log out, then I can reenable the below tweetIt function
-        //     console.log(response);
-        //     // tweetIt(recordString, response);
-        // })
     });
 }
-// function tweetIt(tweetText, b64content) {
+
 function tweetIt(tweetText, tweetImage) {
     // TODO: Run Airtable from within here?
 
@@ -118,36 +111,48 @@ function tweetIt(tweetText, tweetImage) {
 }
 
 // Prepare image
-function prepareImage(imageUrl) {
+function prepareImage(imageUrl, textDate, textLocation, textTitle, textTags) {
     return new Promise((resolve, reject) => {
         // Create the white frame
         // Set to 2048x1024 to match Twitter's preferred 1024x512 ratio
         new Jimp(2048, 1024, '#FFFFFF', (err, frame) => {
             if (err) throw err;
 
+            const maxTextWidth = 662;
+
+            Jimp.loadFont(Jimp.FONT_SANS_32_BLACK).then(font => {
+                frame.print(font, 1166, 280, textDate);
+                frame.print(font, 1166, 360, textLocation, maxTextWidth);
+            });
+            Jimp.loadFont(Jimp.FONT_SANS_16_BLACK).then(font => {
+                frame.print(font, 1166, 512, textTitle, maxTextWidth);
+
+                textTags.map(i => frame.print(font, 1166, 622 + (i * 10), i, maxTextWidth))
+            });
+
+
             // Then read the ephemera image
             Jimp.read(imageUrl, (err, image) => {
-                // if (err) reject(err);
                 if (err) throw err;
 
                 // Resize image to have a maxium dimension of 60%
-                image.scaleToFit(2048, 1024 * 0.60);
+                image.scaleToFit(772, 704);
+
 
                 // Calculate coordinates to center image
-                const x = Math.floor((frame.bitmap.width - image.bitmap.width) / 2);
+                const x = 220;
                 const y = Math.floor((frame.bitmap.height - image.bitmap.height) / 2);
 
                 // Compostite image onto the frame
                 frame.composite(image, x, y)
-                    // Write the final image to file
-                    .write("tweet-img.jpg")
-                    .getBase64(Jimp.MIME_JPEG, (err, src) => {
-                        // Resolve it
-                        resolve(src)
 
+                    // Write the final image to file for debugging
+                    .write("tweet-img.jpg")
+                    // Convert recordImageUrl to base64 for tweeting
+                    .getBase64(Jimp.MIME_JPEG, (err, base64ImageString) => {
+                        // Resolve it
+                        resolve(base64ImageString)
                     })
-                // // Resolve it
-                // resolve("tweet-img.jpg")
             });
         });
 
